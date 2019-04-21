615fe1a099c48b690fe67964d6bcb5e31de2d467
Deeniisz <assis.denis98@gmail.com>
2019-04-20 19:25:04 -0700

    Ok

---
+++ CrawllerMongoDB.py
@@ -0,0 +1,42 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import time
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://127.0.0.1:27017")
+mydb = myclient["crawler"]
+mycol = mydb["products"]
+
+
+print("Qual Pagina deseja Ler:")
+pagina = input()
+
+url = "https://www.carrefour.com.br/busca?termo=Notebook&isGrid=true&sort=&page={}&foodzipzone=na".format(pagina)
+req = urllib.request.Request(url , headers={'User-Agent': 'Mozilla/5.0'})
+
+webpage = urllib.request.urlopen(req).read()
+soup = BeautifulSoup(webpage, "html.parser")
+
+formatedname = soup.findAll("h3", "prd-name")
+formatedprice = soup.findAll("span", "prd-price-new")
+
+name = []
+price = []
+dblist = []
+
+for i in formatedname:
+    line = str(i).replace('<h3 class="prd-name">', "").replace("</h3>", "").strip()
+    name.append(line)
+
+for i in formatedprice:
+    line = str(i).replace('<span class="prd-price-new">', "").replace("<bdi>", "").replace("</bdi>", "").replace("</span>", "").strip()
+    price.append(line)
+
+
+
+i = 0
+for info in name:
+    dblist.append({"name" : name[i], "price" : price[i]})
+    i += 1 
+
+mycol.insert_many(dblist)
